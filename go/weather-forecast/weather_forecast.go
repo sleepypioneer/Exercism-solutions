2d246cb5335cc4559d48bd23257e2c8c46a48ecc
jessica.greene <jessica.greene@ecosia.org>
2023-03-26 19:53:05 +0200

    add lasagna solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides current weather conditions.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current location.
 var CurrentLocation string
 
+// Forecast returns a weather forecast for the given location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
